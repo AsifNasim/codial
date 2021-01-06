4dfa1de2b8c84b1b671481e7d7fa7b27e3720e99
AsifNasim <asifnasimofficial@gmail.com>
2021-01-06 23:27:49 +0530

    Setting up project folder structure.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port  = 8000
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in firing up the server: ${err}`);
+    }
+
+    console.log(`Server is up and running at port : ${port}`)
+})
