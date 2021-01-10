5bf1c72b5b4a8bffc07d05b51d0cac5dcb1baa8e
AsifNasim <asifnasimofficial@gmail.com>
2021-01-11 00:20:12 +0530

    setup static layouts and pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req, res){
-    return res.end('<h1>Profile Page</h1>')
+    return res.render('user_Profile',{
+        title: 'Profile'
+    });
 }
 
 module.exports.contact = function(req, res){
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
-
+const expressLayout = require('express-ejs-layouts');
 const app = express();
+
+// setting up layouts
+app.use(expressLayout);
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
+
+// setting up static files(assets)
+app.use(express.static('./assets'));
 // since we have only one file in routes it will automatically fetch it
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
